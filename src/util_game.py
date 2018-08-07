0b6c5836dd5e9c63c989634b6006c6bf2967e475
didadadida93 <didadadida93@gmail.com>
2018-08-07 12:29:23 +0700

    add check resources function

--- src/util_game.py
+++ src/util_game.py
@@ -14,3 +14,14 @@ def close_welcome_screen(browser: client):
     el = wc.find_element_by_xpath(
         ".//a[@class='closeWindow clickable']")
     browser.click(el)
+
+
+def check_resources(browser: client):
+    resources_list = ["wood", "clay", "iron", "crop"]
+    resources = {}
+    for res in resources_list:
+        find_resources = browser.find("//div[@class='stockContainer {0}']".format(res))
+        find_resources = find_resources.find_element_by_xpath(".//div[contains(@class, 'progressbar')]")
+        value = int(find_resources.get_attribute("value"))
+        resources[res] = value
+    return resources
