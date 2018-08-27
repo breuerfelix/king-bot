69cf83e2f726f3631429a51120e330a74a830f1e
Felix Breuer <f.breuer94@gmail.com>
2018-08-27 10:56:11 +0200

    converted list into enum for easier use

--- src/util_game.py
+++ src/util_game.py
@@ -1,5 +1,6 @@
 from .custom_driver import client
 from .utils import log
+from enum import Enum
 
 
 def close_modal(browser: client):
@@ -20,18 +21,30 @@ def check_resources(browser: client) -> {}:
     resources_list = ["wood", "clay", "iron", "crop"]
     resources = {}
     for res in resources_list:
-        find_resources = browser.find("//div[@class='stockContainer {0}']".format(res))
-        find_resources = find_resources.find_element_by_xpath(".//div[contains(@class, 'progressbar')]")
+        find_resources = browser.find(
+            "//div[@class='stockContainer {0}']".format(res))
+        find_resources = find_resources.find_element_by_xpath(
+            ".//div[contains(@class, 'progressbar')]")
         value = int(find_resources.get_attribute("value"))
         resources[res] = value
     return resources
 
-def shortcut(browser: client, shortcut: str):
-    shortcut_list = ["marketplace", "barrack", "stable", "workshop"]
+
+class shortcut(Enum):
+    marketplace = 0
+    barrack = 1
+    stable = 2
+    workshop = 3
+
+
+def open_shortcut(browser: client, sc: shortcut):
     shortcut_link = browser.find("//div[@id='quickLinks']")
-    shortcut_link = shortcut_link.find_element_by_xpath(".//div[contains(@class, 'slotWrapper')]")
-    link = shortcut_link.find_elements_by_xpath(".//div[contains(@class, 'slotContainer')]")
-    browser.click(link[shortcut_list.index(shortcut.lower())], 1)
+    shortcut_link = shortcut_link.find_element_by_xpath(
+        ".//div[contains(@class, 'slotWrapper')]")
+    link = shortcut_link.find_elements_by_xpath(
+        ".//div[contains(@class, 'slotContainer')]")
+    browser.click(link[sc.value], 1)
+
 
 def village_list(browser: client) -> []:
     villages_list = []
@@ -39,7 +52,8 @@ def village_list(browser: client) -> []:
     ul = ul.find_element_by_xpath(".//ul")
     lis = ul.find_elements_by_xpath(".//li")
     for village in lis:
-        village_name = village.find_element_by_xpath(".//div[contains(@class, 'villageEntry')]")
+        village_name = village.find_element_by_xpath(
+            ".//div[contains(@class, 'villageEntry')]")
         village_name = village_name.get_attribute("innerHTML")
         villages_list.append(village_name)
     return villages_list
